cfc6ec81432cbb988f92856982f5f54d90dc9294
farzinads <farzinahmadi1360@gmail.com>
2026-05-30 15:32:49 +0200

    Add files via upload

---
+++ README_DE.txt
@@ -0,0 +1,14 @@
+Dominos Arbeitszeit – PWA Version
+
+Diese Version ist für iPhone Home Screen vorbereitet.
+
+Wichtig:
+- Direkt als lokale Datei funktioniert die Home-Screen-App nur eingeschränkt.
+- Für die beste Nutzung muss dieser Ordner online veröffentlicht werden, z. B. mit Vercel oder Netlify.
+- Danach den Link im iPhone mit Safari öffnen und Teilen > Zum Home-Bildschirm wählen.
+
+Dateien:
+- index.html
+- manifest.webmanifest
+- service-worker.js
+- icon-180.png / icon-192.png / icon-512.png
---
+++ service-worker.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'dominos-arbeitszeit-v1';
+const CORE_ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request).then(r => r || caches.match('./index.html'))));
+});
